540d31d2affb66c6fd1c7b9829391355524dea68
stoyantodorovbg <stoyan_11@abv.bg>
2017-11-29 12:08:38 +0200

    Add article and user-role functionalitu to Blog

--- Blog-PHP-and-SymfonyExercise/src/AppBundle/Controller/ArticleController.php
+++ Blog-PHP-and-SymfonyExercise/src/AppBundle/Controller/ArticleController.php
@@ -4,11 +4,13 @@ namespace AppBundle\Controller;
 
 use AppBundle\Entity\Article;
 use AppBundle\Form\ArticleType;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Response;
 
 
 class ArticleController extends Controller
@@ -41,6 +43,87 @@ class ArticleController extends Controller
             ['form' => $form->createView()]);
     }
 
+    /**
+     * @Route("/article/{id}", name="article_view")
+     *
+     * @param $id
+     * @return Response
+     */
+    public function viewArticle($id)
+    {
+        $article = $this->getDoctrine()->getRepository(Article::class)->find($id);
 
+        return $this->render('article/article.html.twig', ['article' => $article]);
+    }
+
+    /**
+     * @Route("/article/edit/{id}", name="article_edit")
+     * @Security("is_granted('IS_AUTHENTICATED_FULLY')")
+     *
+     * @param $id
+     * @param Request $request
+     * @return Response
+     */
+    public function editArticle($id, Request $request)
+    {
+        $article = $this->getDoctrine()->getRepository(Article::class)->find($id);
+
+        if ($article === null) {
+            return $this->redirectToRoute('homepage');
+        }
+
+        $form = $this->createForm(ArticleType::class, $article);
+        $form->handleRequest($request);
+
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($article);
+            $em->flush();
+
+            return $this->redirectToRoute('article_view',
+            ['id' => $article->getId()]);
+        }
+
+        return $this->render('article/edit.html.twig', [
+            'form' => $form->createView(),
+            'article' => $article
+        ]);
+    }
+
+    /**
+     * @Route("/article/delete/{id}", name="article_delete")
+     * @Security("is_granted('IS_AUTHENTICATED_FULLY')")
+     *
+     * @param $id
+     * @param Request $request
+     * @return Response
+     */
+    public function delete($id, Request $request)
+    {
+
+        $article = $this->getDoctrine()->getRepository(Article::class)->find($id);
+
+        if ($article === null) {
+            return $this->redirectToRoute('homepage');
+        }
+
+        $form = $this->createForm(ArticleType::class, $article);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($article);
+            $em->flush();
+
+            return $this->redirectToRoute('homepage');
+        }
+
+        return $this->render('article/delete.html.twig', [
+            'form' => $form->createView(),
+            'article' => $article
+        ]);
+    }
 
 }
--- Blog-PHP-and-SymfonyExercise/src/AppBundle/Entity/User.php
+++ Blog-PHP-and-SymfonyExercise/src/AppBundle/Entity/User.php
@@ -52,11 +52,22 @@ class User implements UserInterface
     private $articles;
 
     /**
+     * @var ArrayCollection
+     *
+     * @ORM\ManyToMany(targetEntity="AppBundle\Entity\Role")
+     * @ORM\JoinTable(name="users_roles",
+     *     joinColumns={@ORM\JoinColumn(name="user_id", referencedColumnName="id")},
+     *     inverseJoinColumns={@ORM\JoinColumn(name="role_id", referencedColumnName="id")})
+     */
+    private $roles;
+
+    /**
      * User constructor.
      */
     public function __construct()
     {
         $this->articles = new ArrayCollection();
+        $this->roles = new ArrayCollection();
     }
 
 
@@ -147,7 +158,24 @@ class User implements UserInterface
      */
     public function getRoles()
     {
-        return [];
+        $stringRoles = [];
+        foreach ($this->roles as $role) {
+            /** @var $role Role */
+            $stringRoles[] = $role->getRole();
+        }
+        return $stringRoles;
+    }
+
+    /**
+     * @param \AppBundle\Entity\Role $role
+     *
+     * @return User
+     */
+    public function addRole(Role $role)
+    {
+        $this->roles[] = $role;
+
+        return $this;
     }
 
     public function getSalt()
